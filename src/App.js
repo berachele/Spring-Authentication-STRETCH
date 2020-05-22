a05935b1b41d9b168068e4f8cbde694876c35b38
Rachele Edwards <dot.com1472@gmail.com>
2020-05-22 11:55:08 -0600

    Routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import PrivateRoute from './components/PrivateRoute'
 import './App.css';
+import Login from './components/Login';
+import Signup from "./components/Signup"
+import Jokes from "./components/Jokes"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Route exact path='/' component={Signup}/>
+        <Route exact path='/login' component={Login}/>
+        <PrivateRoute path='/jokes' component={Jokes} />
       </header>
     </div>
   );
